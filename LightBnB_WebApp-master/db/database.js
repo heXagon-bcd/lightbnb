4c1b8c4155fe8e07454a1b6df721a6f9da3848ae
Shawn He <shawnhe1991@gmail.com>
2023-06-15 23:28:16 -0400

    connect first funct to database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,7 +1,17 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'shawnhe',
+  password: '',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
+
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
 
 /**
  * Get a single user from the database given their email.
@@ -59,13 +69,28 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const queryString = `
+select * from properties LIMIT $1;
+`
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(queryString, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
+// const getAllProperties = function (options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// };
 
 /**
  * Add a property to the database
